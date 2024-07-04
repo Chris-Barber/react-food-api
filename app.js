12b07386bc37b5201126ccac491a66edb350c6e3
Chris Barber <chris.barber@moat.co.uk>
2024-07-04 17:44:23 +0100

    Default port number

--- app.js
+++ app.js
@@ -70,4 +70,8 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
 });
 
-app.listen(3000);
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
